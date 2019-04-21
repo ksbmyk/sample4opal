22172cba78cb3e54cfbb3315ccdf8d51595ac3fb
Miyuki Koshiba <ksbmyk@gmail.com>
2019-04-21 19:50:23 +0900

    Add button of sounds

--- app/application.rb
+++ app/application.rb
@@ -4,8 +4,33 @@ require 'hyalite'
 class AppView
   include Hyalite::Component
 
+  def play
+    %x(
+      var context = new AudioContext();
+      var buffer = null;
+      var source = context.createBufferSource();
+
+      var request = new XMLHttpRequest();
+      request.open('GET', 'assets/sounds/sample.mp3', true);
+      request.responseType = 'arraybuffer';
+      request.send();
+
+      request.onload = function() {
+        var res = request.response;
+        context.decodeAudioData(res, function(buf) {
+          source.buffer = buf;
+        });
+      };
+      source.connect(context.destination);
+      source.start(0);
+    )
+  end
+
   def render
-    h2(nil, 'Welcome!!!')
+    div(nil,
+      h2(nil, 'Welcome Web Audio'),
+        button({onClick: self.method(:play)}, 'Play')
+    )
   end
 end
 Hyalite.render(Hyalite.create_element(AppView), $document['.content'])
